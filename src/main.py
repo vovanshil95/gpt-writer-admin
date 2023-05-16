186655f18d2e8c0a727e0256b55fc7852a568909
vovanshil95 <vovanshil95@mail.ru>
2023-05-16 14:55:17 +0300

    fixed bugs

--- src/main.py
+++ src/main.py
@@ -7,12 +7,16 @@ import os
 app = FastAPI()
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+class Match(BaseModel):
+    question: str
+    answer: str
+
 class Prompt(BaseModel):
-    text: str
+    matches: list[Match]
 
 
 @app.post('/api')
 def get_gpt_answer(prompt: Prompt):
-    response = openai.ChatCompletion.create(model='gpt-4', messages=[{'role': 'user', 'content': prompt.text}])
+    response = openai.ChatCompletion.create(model='gpt-4', messages=[{'role': 'user', 'content': '\n\n'.join(lambda m: f'{m.question}: {m.answer}'.aprompt.matches)}])
     answer = response['choices'][0]['message']['content']
     return {'status': 200, 'data': {'answer': answer}}
