a011035b66e00294171ab6d774c54594e6484e10
vovanshil95 <vovanshil95@mail.ru>
2023-05-25 20:16:56 +0300

    add favorite prompts

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sqlalchemy import create_engine, func
@@ -16,16 +18,18 @@ class MatchSchema(BaseModel):
     id: uuid.UUID
     question: str
     answer: str
-class ResponseMatch(BaseResponse):
+    color: str
+
+class MatchResponse(BaseResponse):
     data: list[MatchSchema]
 
 class PromptsSchema(BaseModel):
     prompt: list[str]
-class ResponsePrompts(BaseResponse):
+class PromptsResponse(BaseResponse):
     data: PromptsSchema
 
 class InteractionSchema(BaseModel):
-    prompts: list[str]
+    prompt: list[str]
     gpt_response: str
     datetime: datetime.datetime
 
@@ -38,20 +42,31 @@ class GptAnswerSchema(BaseModel):
 class GptAnswerResponse(BaseResponse):
     data: GptAnswerSchema
 
+class FavoritePromptShema(BaseModel):
+    id: uuid.UUID
+    title: str
+    prompt: list[str]
+
+class FavoritePromptResponse(BaseResponse):
+    data: list[FavoritePromptShema]
+
 app = FastAPI()
 
 sqlalchemy_session = sessionmaker(create_engine(sqlalchemy_url))
 openai.api_key = OPENAI_API_KEY
 
 @app.get('/api/questions')
-def get_questions():
+def get_questions() -> MatchResponse:
     with sqlalchemy_session.begin() as session:
         matches = session.query(Match).all()
-        matches = list(map(lambda m: {'id': str(m.id), 'question': m.question, 'answer': m.answer, 'color': m.color}, matches))
+        matches = list(map(lambda m: MatchSchema(id=str(m.id),
+                                                 question=m.question,
+                                                 answer=m.answer,
+                                                 color=m.color), matches))
     return {'status': 'success', 'message': 'Questions successfully retrieved', 'data': matches}
 
 @app.put('/api/questions')
-def put_questions(questions: list[MatchSchema]) -> ResponseMatch:
+def put_questions(questions: list[MatchSchema]) -> MatchResponse:
     with sqlalchemy_session.begin() as session:
         session.query(Match).delete()
         session.add_all(map(lambda m: Match(m.id, m.question, m.answer, m.color), questions))
@@ -59,28 +74,29 @@ def put_questions(questions: list[MatchSchema]) -> ResponseMatch:
 
 
 @app.get('/api/prompt')
-def get_prompt():
+def get_prompt() -> PromptsResponse:
     with sqlalchemy_session.begin() as session:
         prompts = session.query(PromptBlank).all()
-        prompts = list(map(lambda q: {'id': q.id, 'text_data': q.text_data}, prompts))
-    return {'status': 'success', 'message': 'Prompt successfully retrieved', 'data': {'prompt': prompts}}
+        prompts = PromptsSchema(prompt=list(map(lambda q: q.text_data, prompts)))
+    return {'status': 'success', 'message': 'Prompt successfully retrieved', 'data': prompts}
 
 
 @app.put('/api/prompt')
-def put_prompt(prompts: list[str]):
+def put_prompt(prompts: PromptsSchema) -> PromptsResponse:
     with sqlalchemy_session.begin() as session:
-        session.add_all(list(map(lambda pr: PromptBlank(uuid.UUID(hex=str(uuid.uuid4())), pr), prompts)))
-    return {'status': 'success', 'message': 'Prompt successfully saved', 'prompt': prompts}
+        session.query(PromptBlank).delete()
+        session.add_all(list(map(lambda pr: PromptBlank(uuid.UUID(hex=str(uuid.uuid4())), pr), prompts.prompt)))
+    return {'status': 'success', 'message': 'Prompt successfully saved', 'data': prompts}
 
 
 @app.get('/api/history')
-def get_history():
+def get_history() -> InteractionsResponse:
     with sqlalchemy_session.begin() as session:
         history = session.query(GptInteraction, func.array_agg(FilledPrompt.text_data))\
             .join(FilledPrompt).group_by(GptInteraction.id).all()
-        history = list(map(lambda el: {'prompt': el[1],
-                                       'gpt_response': el[0].gpt_answer,
-                                       'datetime': el[0].time_happened}, history))
+        history = list(map(lambda el: InteractionSchema(prompt=el[1],
+                                                        gpt_response=el[0].gpt_answer,
+                                                        datetime=el[0].time_happened), history))
     return {"status": "success", "message": "History successfully retrieved", 'data': history}
 
 
@@ -94,3 +110,27 @@ def get_response(prompts: list[str]) -> GptAnswerResponse:
         session.flush()
         session.add_all(map(lambda pr: FilledPrompt(uuid.UUID(hex=str(uuid.uuid4())), pr, interaction_id), prompts))
     return {'status': 'success', 'message': 'GPT Respons successfully retrieved', 'data': {'gpt_response': answer}}
+
+@app.get('/api/favoritesPrompts')
+def get_favorite_prompts() -> FavoritePromptResponse:
+    with sqlalchemy_session.begin() as session:
+        favorite_prompts = session.query(FavoritePrompt, func.array_agg(FavoritePromptBlank.text_data))\
+            .join(FavoritePromptBlank).group_by(FavoritePrompt.id).all()
+        favorite_prompts = list(map(lambda p: FavoritePromptShema(id=p[0].id,
+                                                                  title=p[0].title,
+                                                                  prompt=p[1]), favorite_prompts))
+    return {'status': 'success', 'message': 'Favorite prompts successfully retrieved', 'data': favorite_prompts}
+
+@app.post('/api/favoritesPrompts')
+def post_favorite_prompts(prompts: list[FavoritePromptShema]) -> FavoritePromptResponse:
+    with sqlalchemy_session.begin() as session:
+        session.add_all(map(lambda p: FavoritePrompt(id=p.id, title=p.title), prompts))
+        session.flush()
+        prompt_blanks = []
+        for p in prompts:
+            prompt_blanks.extend([FavoritePromptBlank(id=uuid.UUID(hex=str(uuid.uuid4())),
+                                                         favorite_prompt_id=pb[0],
+                                                         text_data=pb[1])
+                                     for pb in zip([p.id] * len(p.prompt), p.prompt)])
+        session.add_all(prompt_blanks)
+    return {'status': 'success', 'message': 'Favorite prompts successfully saved', 'data': prompts}
